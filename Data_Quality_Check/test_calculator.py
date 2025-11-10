8107d9a5a7908344bb506e9a9136346d0aec6c95
deshmukhor9 <156787769+deshmukhor9@users.noreply.github.com>
2025-11-10 12:05:19 +0530

    etl pipeline code coverage

--- Data_Quality_Check/test_calculator.py
+++ Data_Quality_Check/test_calculator.py
@@ -2,18 +2,18 @@ from calculator import add, sub, mul, div
 import pytest
 # import pytest_cov
 
-def test_add(a,b):
+def test_add():
     assert add(10,5) == 15
 
-def test_sub(a,b):
+def test_sub():
     assert sub(10,5) == 5
 
-def test_mul(a,b):
+def test_mul():
     assert mul(10,5) == 50
 
-def test_div1(a,b):
+def test_div1():
     assert div(10,5) == 2
 
-def test_div2(a,b):
+def test_div2():
     assert div(10,0) == 10
 
